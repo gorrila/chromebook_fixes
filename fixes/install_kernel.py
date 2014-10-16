eb36000eb9110ac208a1ffdd4fa403e6dc4bb89e
Sinan Böcker <sinan.boecker@traveltainment.de>
2014-10-16 16:47:47 +0200

    - Kernel install output should be better

--- fixes/install_kernel.py
+++ fixes/install_kernel.py
@@ -4,7 +4,6 @@ import urllib
 import os
 import platform
 import subprocess
-import getpass
 
 
 def install_3_17():
@@ -15,7 +14,7 @@ def install_3_17():
         update_kernel = True
 
     if update_kernel:
-        print("Grabbing kernel 3.17 stable...may take a few moments")
+        print("\nGrabbing kernel 3.17 stable...may take a few moments")
         kernel = urllib.URLopener()
         # Check if system is 32 or 64-bit
         if platform.architecture()[0] is "64bit":
@@ -26,10 +25,10 @@ def install_3_17():
             kernel.retrieve("http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.17-utopic/linux-image-3.17.0-031700-generic_3.17.0-031700.201410060605_i386.deb", "/tmp/linux-image-3.17.0-031700-generic_3.17.0-031700.201410060605_i386.deb")
         kernel.retrieve("http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.17-utopic/linux-headers-3.17.0-031700_3.17.0-031700.201410060605_all.deb", "/tmp/linux-headers-3.17.0-031700_3.17.0-031700.201410060605_all.deb")
 
-        print("Remove old kernel")
+        print("\nRemove old kernel")
         os.system("""apt-get remove --purge $(dpkg -l 'linux-image-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d')""")
 
-        print("Extract packages to update the kernel")
+        print("\nInstall kernel and delete .deb files\n")
         os.system("dpkg -i /tmp/linux-headers-3.17*.deb")
         os.system("dpkg -i /tmp/linux-image-3.17*.deb")
 
